22a0ab9c939828ac00ca87dba1966119ac5ea66f
shubhammane2082 <shubhammane2082@gmail.com>
2024-03-25 13:08:47 +0530

    Perform simple math operation by using Java 8 Features like Lambda Expression and Functional Interface

---
+++ CalculatorByLambdaExpression/src/com/java8/Features/lambdaExpression/MathCalculatorByLambdaExpression.java
@@ -0,0 +1,18 @@
+package com.java8.Features.lambdaExpression;
+
+public class MathCalculatorByLambdaExpression {
+
+	public static void main(String[] args) 
+	{
+		Mathoperations addition=(a,b) -> a+b;
+		Mathoperations substraction=(a,b) -> a-b;
+		Mathoperations multiplication=(a,b) -> a*b;
+		Mathoperations division=(a,b) -> a+b;
+		
+		System.out.println("Addition is : "+addition.calculate(10, 5));
+		System.out.println("Substraction is : "+substraction.calculate(10, 5));
+		System.out.println("Multiplication is : "+multiplication.calculate(10, 5));
+		System.out.println("Division is : "+division.calculate(10, 5));
+	}
+
+}
---
+++ CalculatorByLambdaExpression/src/com/java8/Features/lambdaExpression/Mathoperations.java
@@ -0,0 +1,6 @@
+package com.java8.Features.lambdaExpression;
+
+@FunctionalInterface
+public interface Mathoperations {
+	int calculate(int a,int b);
+}
