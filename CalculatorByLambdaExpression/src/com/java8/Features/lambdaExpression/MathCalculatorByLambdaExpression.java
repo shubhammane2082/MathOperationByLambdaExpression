b12e44b3f2b4574a61972a455491def5191d9140
shubhammane2082 <shubhammane2082@gmail.com>
2024-03-25 13:16:42 +0530

    Print the all results of operations by providing static method in functional interface

--- CalculatorByLambdaExpression/src/com/java8/Features/lambdaExpression/MathCalculatorByLambdaExpression.java
+++ CalculatorByLambdaExpression/src/com/java8/Features/lambdaExpression/MathCalculatorByLambdaExpression.java
@@ -13,6 +13,13 @@ public class MathCalculatorByLambdaExpression {
 		System.out.println("Substraction is : "+substraction.calculate(10, 5));
 		System.out.println("Multiplication is : "+multiplication.calculate(10, 5));
 		System.out.println("Division is : "+division.calculate(10, 5));
+		
+		System.out.println();
+		
+		Mathoperations.printResult(10, 5, addition, "Addition");
+		Mathoperations.printResult(10, 5, substraction, "substraction");
+		Mathoperations.printResult(10, 5, multiplication, "multiplication");
+		Mathoperations.printResult(10, 5, division, "division");
 	}
 
 }
--- CalculatorByLambdaExpression/src/com/java8/Features/lambdaExpression/Mathoperations.java
+++ CalculatorByLambdaExpression/src/com/java8/Features/lambdaExpression/Mathoperations.java
@@ -3,4 +3,9 @@ package com.java8.Features.lambdaExpression;
 @FunctionalInterface
 public interface Mathoperations {
 	int calculate(int a,int b);
+	
+	public static void printResult(int a,int b,Mathoperations operations,String function)
+	{
+		System.out.println("Result of "+function+" is : "+operations.calculate(a, b));
+	}
 }
